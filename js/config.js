d43cb982d9d6587fe6b9269ede1105cd94c477f2
hyun <10jqk1@kakao.com>
2024-09-21 16:16:18 +0900

    config edit

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-northeast-2_5O1oQ5e9z', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1n8q0ef2s6dhsdvmdn0kktp740', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-northeast-2_1Lsre40Fl', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1ql3dohc4i634pjisfj3osh6gr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://hwdro2am04.execute-api.ap-northeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
